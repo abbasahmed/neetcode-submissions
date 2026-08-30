1c741ea5b474cc91e6531b2b98fc28f65bb6a805
Abbas Ahmed <aza16@pitt.edu>
2026-08-30 02:12:59 -0400

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = [-1] * (n + 1)
+        memo[0] = 1
+        memo[1] = 1
+        print(len(memo))
+
+        for i in range(2, n + 1):
+            memo[i] = memo[i - 1] + memo [i - 2]
+        
+        return memo[n]
