62572d3372becb15ca5297c9884fff6b0bfa6f32
Abbas Ahmed <ABBASZAHMED@GMAIL.COM>
2026-09-08 00:40:37 -0400

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = list(zip(position, speed))
+        cars.sort(reverse=True)
+        fleets = 0
+        last_eta = -math.inf
+        for (p, s) in cars:
+            eta = (target - p) / s
+            if eta > last_eta:
+                fleets += 1
+                last_eta = eta
+        return fleets
+
